d125d02e7db8251ad9fa0619807f47553e156a14
shin <146090073+shitom31021@users.noreply.github.com>
2023-11-15 16:51:33 +0900

    app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def communicate():
     messages.append(user_message)
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-1106",
+        model="gpt-4",
         messages=messages
     )  
 
